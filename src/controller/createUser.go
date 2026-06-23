48721fd4eca114dd0dd4571ea9516e8aa7cada27
RafaelFleitas <rafaelportugallaeber@gmail.com>
2026-06-23 09:33:58 -0400

    feat: Criação do createUser

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,12 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/RafaelFleitas/API-Golang/src/configuration/rest_err"
+	"github.com/RafaelFleitas/API-Golang/src/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 // Controller do Create
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("Existem campos incorretos, error %s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+
 }
